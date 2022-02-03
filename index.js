4afe1570ca9bc4c473ce596bb13c699e6d70576d
Pugazh Krishna <sridevibagavathi@gmail.com>
2022-02-03 15:48:22 +0530

    cors added

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ import postLoginRoutes from "./routes/postLoginRoutes";
 import momentsRoutes from "./routes/momentsRoutes";
 import bodyParser from "body-parser";
 import auth from "./middleware/auth";
-const PORT = process.env.PORT;
-const urlencodedParser = bodyParser.urlencoded({ extended: false }); // to support URL-encoded bodies
+import cors from "cors";
 
+const PORT = process.env.PORT;
+const urlencodedParser = bodyParser.urlencoded({ extended: true }); // to support URL-encoded bodies
+app.use(bodyParser.json())
+app.use(cors({
+  origin: '*'
+}));
+app.use(cors({
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
 app.use(compression()); //Compress all routes
 app.use("/", urlencodedParser, preLoginRoutes);
 app.use(auth);
